5921e485e0a9e1059fa0efd6ef4c0898d18a10b6
rawazkader85 <rkade103@uottawa.ca>
2018-11-19 23:26:28 -0500

    Worked till lecture 102. Added more enemy paths and looping waves of ennemies.

--- Assets/Scripts/Shredder.cs
+++ Assets/Scripts/Shredder.cs
@@ -6,6 +6,6 @@ public class Shredder : MonoBehaviour {
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        DestroyObject(collision.gameObject);
+        Destroy(collision.gameObject);
     }
 }
